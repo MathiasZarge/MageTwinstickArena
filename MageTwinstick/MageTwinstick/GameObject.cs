def202f872364eeb7843379b26493ca8716231ed
blorsten <peter.witt.joergensen@gmail.com>
2015-11-11 12:13:32 +0100

    Added fields, properties, constructer and methods for gameworld + fields properties and constructer for gameobject

--- MageTwinstick/MageTwinstick/GameObject.cs
+++ MageTwinstick/MageTwinstick/GameObject.cs
@@ -1,7 +1,25 @@
-﻿namespace MageTwinstick
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace MageTwinstick
 {
     abstract class GameObject
     {
-         
+        //fields
+        protected Image sprite;
+        protected Rectangle display;
+        protected List<Image> animationFrames;
+        protected float currentFrameIndex;
+        private RectangleF collisionBox;
+
+        //Properteis
+        public Vector2D Position { get; set; }
+        public RectangleF CollisionBox { get; }
+
+        //Constructer
+        public GameObject(string imagePath, Vector2D startPos, Rectangle display, float animationSpeed)
+        {
+            
+        }
     }
 }
--- MageTwinstick/MageTwinstick/GameWorld.cs
+++ MageTwinstick/MageTwinstick/GameWorld.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using IrrKlang;
 
@@ -12,6 +13,49 @@ namespace MageTwinstick
         private float currentFps;
         private BufferedGraphics backBuffer;
         private Rectangle display;
-        private static ISoundEngine soundEngine;
+
+        //Properties
+        //Auto properties for the given values
+        public static ISoundEngine SoundEngine { get; } = new ISoundEngine();
+        public List<GameObject> Objects { get; set; }
+        public List<GameObject> ObjectsToRemove { get; set; }
+        public List<GameObject> ObjectsToAdd { get; set; }
+
+        //Constructer
+        public GameWorld(Graphics dc, Rectangle display) //takes graphics and display as arguments
+        {
+            this.display = display;
+            this.backBuffer = BufferedGraphicsManager.Current.Allocate(dc, display);
+
+            this.dc = backBuffer.Graphics;
+        }
+
+        //Methods
+        public void SetupWorld() // Setup the world before we begin the game loop
+        {
+            
+        }
+
+        public void GameLoop()
+        {
+            Update(); // Update all gameobjects
+            UpdateAnimation(); // update all animations
+            Draw(); // draw all objects
+        }
+
+        public void Draw()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void UpdateAnimation()
+        {
+            
+        }
     }
 }
