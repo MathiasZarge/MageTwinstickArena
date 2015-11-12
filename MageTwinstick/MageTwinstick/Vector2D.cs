b1f73969356d836b9e48de2483147d5ad29e3f33
NearlyBatman <denskyldige@hotmail.com>
2015-11-12 09:58:57 +0100

    Vector comments

--- MageTwinstick/MageTwinstick/Vector2D.cs
+++ MageTwinstick/MageTwinstick/Vector2D.cs
@@ -5,18 +5,18 @@ namespace MageTwinstick
 {
     class Vector2D
     {
-        //
+        //Properties for X and Y coordinates.
         public float X { get; set; }
         public float Y { get; set; }
 
-        //
+        //Creates a vector from two floats representing x and y.
         public Vector2D (float x, float y)
         {
             X = x;
             Y = y;
         }
     
-        //
+        //Finds the difference between one position and another by creating a vector between them.
         public Vector2D Subtract(Vector2D vec)
         {
             Vector2D newVec = new Vector2D(vec.X - this.X, vec.Y - this.Y);
@@ -24,13 +24,13 @@ namespace MageTwinstick
             return newVec;
         }
 
-        //
+        //Calculates the lenght of a vector.
         private float Length()
         {
             return (float)Math.Sqrt((this.X * this.X) + (this.Y * this.Y));
         }
 
-        //Normalizes the vector
+        //Normalizes the vector.
         public void Normalize()
         {
             float length = Length();
