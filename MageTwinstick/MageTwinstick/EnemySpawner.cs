3dadbb64a011f51ca4d509c257b7e2ab250d05d6
NearlyBatman <denskyldige@hotmail.com>
2015-11-17 13:21:06 +0100

    MoreSpawner

--- MageTwinstick/MageTwinstick/EnemySpawner.cs
+++ MageTwinstick/MageTwinstick/EnemySpawner.cs
@@ -5,15 +5,21 @@ using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
 
+
 namespace MageTwinstick
 {
     class EnemySpawner
     {
+
         private float time = 0;
         private int spawnCounter = 1;
         List<GameObject> enemy = new List<GameObject>();
         private Rectangle display;
         private Player player;
+        protected float angle;
+        protected float posX;
+        protected float posY;
+        private Random rnd = new Random();
         List<bool> spawned = new List<bool> { false, false, false, false };
 
         public EnemySpawner(Rectangle display, Player player)
@@ -25,21 +31,32 @@ namespace MageTwinstick
         //Updates to add enemies onto the field
         public virtual void Update(float fps)
         {
-            
-            time += (1/fps);
+
+            //Used for deciding the time and what time for enemies to spawn.
+            time += (1 / fps);
+            //Checking time to see if it is time for this type of enemy to spawn.
             if (time >= 1 && !spawned[0])
             {
+                //Making sure a greater number of this type of enemy is spawning with every passing round.
                 for (int i = 0; i < spawnCounter; i++)
                 {
-                    GameWorld.ObjectsToAdd.Add(new Enemy(100, 75, @"Images\Crawler\Move\0.png;Images\Crawler\Move\1.png;Images\Crawler\Move\2.png;Images\Crawler\Move\3.png;Images\Crawler\Move\4.png;Images\Crawler\Move\5.png;Images\Crawler\Move\6.png;Images\Crawler\Move\7.png", new Vector2D(300, 0), display, 15, 0, 2.0f, player));
+                    //Creates this type of enemy in the game.
+                    angle = (float)(rnd.NextDouble() * 360);
+                    posX = 600f * (float)Math.Cos(angle) + display.Width / 2;
+                    posY = 600f * (float)Math.Sin(angle) + display.Height / 2;
+                    GameWorld.ObjectsToAdd.Add(new Enemy(100, 75, @"Images\Crawler\Move\0.png;Images\Crawler\Move\1.png;Images\Crawler\Move\2.png;Images\Crawler\Move\3.png;Images\Crawler\Move\4.png;Images\Crawler\Move\5.png;Images\Crawler\Move\6.png;Images\Crawler\Move\7.png", new Vector2D(posX, posY), display, 15, 0, 2.0f, player));
                 }
                 spawned[0] = true;
             }
+            //Most of the functions below are the same as the above.
             if (time >= 5 && !spawned[1])
             {
                 for (int i = 0; i < spawnCounter; i++)
                 {
-                    GameWorld.ObjectsToAdd.Add(new Enemy(125, 50, @"Images\Scorpion\Move\0.png;Images\Scorpion\Move\1.png;Images\Scorpion\Move\2.png;Images\Scorpion\Move\3.png", new Vector2D(900, 0), display, 20, 0, 2.0f, player));
+                    angle = (float)(rnd.NextDouble() * 360);
+                    posX = 600f * (float)Math.Cos(angle) + display.Width / 2;
+                    posY = 600f * (float)Math.Sin(angle) + display.Height / 2;
+                    GameWorld.ObjectsToAdd.Add(new Enemy(125, 50, @"Images\Scorpion\Move\0.png;Images\Scorpion\Move\1.png;Images\Scorpion\Move\2.png;Images\Scorpion\Move\3.png", new Vector2D(posX, posY), display, 20, 0, 2.0f, player));
                 }
                 spawned[1] = true;
             }
@@ -47,7 +64,10 @@ namespace MageTwinstick
             {
                 for (int i = 0; i < spawnCounter; i++)
                 {
-                    GameWorld.ObjectsToAdd.Add(new Enemy(50, 125, @"Images\Demon\Move\0.png;Images\Demon\Move\1.png;Images\Demon\Move\2.png;Images\Demon\Move\3.png;Images\Demon\Move\4.png;Images\Demon\Move\5.png;Images\Demon\Move\6.png;Images\Demon\Move\7.png", new Vector2D(600, 0), display, 10, 0, 2.0f, player));
+                    angle = (float)(rnd.NextDouble() * 360);
+                    posX = 600f * (float)Math.Cos(angle) + display.Width / 2;
+                    posY = 600f * (float)Math.Sin(angle) + display.Height / 2;
+                    GameWorld.ObjectsToAdd.Add(new Enemy(50, 125, @"Images\Demon\Move\0.png;Images\Demon\Move\1.png;Images\Demon\Move\2.png;Images\Demon\Move\3.png;Images\Demon\Move\4.png;Images\Demon\Move\5.png;Images\Demon\Move\6.png;Images\Demon\Move\7.png", new Vector2D(posX, posY), display, 10, 0, 2.0f, player));
                 }
                 spawned[2] = true;
             }
@@ -55,7 +75,10 @@ namespace MageTwinstick
             {
                 for (int i = 0; i < spawnCounter; i++)
                 {
-                    GameWorld.ObjectsToAdd.Add(new Enemy(50, 200, @"Images\Gargant\Move\0.png;Images\Gargant\Move\1.png;Images\Gargant\Move\2.png;Images\Gargant\Move\3.png;Images\Gargant\Move\4.png;Images\Gargant\Move\5.png;Images\Gargant\Move\6.png;Images\Gargant\Move\7.png", new Vector2D(0, 0), display, 10, 0, 2.0f, player));
+                    angle = (float)(rnd.NextDouble() * 360);
+                    posX = 600f * (float)Math.Cos(angle) + display.Width / 2;
+                    posY = 600f * (float)Math.Sin(angle) + display.Height / 2;
+                    GameWorld.ObjectsToAdd.Add(new Enemy(50, 200, @"Images\Gargant\Move\0.png;Images\Gargant\Move\1.png;Images\Gargant\Move\2.png;Images\Gargant\Move\3.png;Images\Gargant\Move\4.png;Images\Gargant\Move\5.png;Images\Gargant\Move\6.png;Images\Gargant\Move\7.png", new Vector2D(posX, posY), display, 10, 0, 2.0f, player));
                 }
                 spawned[0] = false;
                 spawned[1] = false;
