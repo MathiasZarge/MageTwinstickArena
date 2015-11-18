e44819c69a0c4c0b789fd28cdae06968912859a8
MathiasZarge <mathias_hadsten@hotmail.com>
2015-11-18 11:07:01 +0100

    Added Doxygen readable comments to GameObject and updated Doxygen

---
+++ MageTwinstick/MageTwinstick/html/search/all_c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['unit',['Unit',['../class_mage_twinstick_1_1_unit.html',1,'MageTwinstick']]],
+  ['unit',['Unit',['../class_mage_twinstick_1_1_unit.html#a857d0f6e0f55b4c81e2a3bbf8711dd18',1,'MageTwinstick::Unit']]],
+  ['update',['Update',['../class_mage_twinstick_1_1_game_object.html#a3de8248d06d234f8335525bbb28ccacc',1,'MageTwinstick.GameObject.Update()'],['../class_mage_twinstick_1_1_player.html#a5324e0350784da0f66369ec5ef52516a',1,'MageTwinstick.Player.Update()'],['../class_mage_twinstick_1_1_projectile.html#a9208eff25bc92289191d5470bbc7015a',1,'MageTwinstick.Projectile.Update()']]],
+  ['updateanimation',['UpdateAnimation',['../class_mage_twinstick_1_1_game_object.html#a35d6d9af3335b978c618ab73da1215a0',1,'MageTwinstick::GameObject']]]
+];
---
+++ MageTwinstick/MageTwinstick/html/search/properties_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['position',['Position',['../class_mage_twinstick_1_1_game_object.html#a74cf54e9808f3a9f93fa5c837076a7f0',1,'MageTwinstick::GameObject']]]
+];
